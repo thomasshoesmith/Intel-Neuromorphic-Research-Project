f70c6158addfbf0be70d2cf6a68eb10f32b9ce74
Thomas Shoesmith <thomas.shoesmith@yahoo.com>
2024-01-09 01:41:31 +0000

    Jade scan

--- jade_parallel_search.sh
+++ jade_parallel_search.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 
 # set max wallclock time
-#SBATCH --time=24:00:00
+#SBATCH --time=00:05:00
 
 # set name of job
 #SBATCH --job-name=job1
@@ -16,22 +16,22 @@
 #SBATCH --partition=small
 
 # mail alert at start, end and abortion of execution
-#SBATCH --mail-type=ALL
+# SBATCH --mail-type=ALL
 
 # send mail to this address
-#SBATCH --mail-user=ts468@sussex.ac.uk
+# SBATCH --mail-user=ts468@sussex.ac.uk
 
 # run the application
 # cd ~/PhD/Intel-Neuromorphic-Research-Project/
 # python HD_eventprop_real_time.py jade_test_params_02/params_${SLURM_ARRAY_TASK_ID}.json
+# sbatch --array=1-30 jade_parallel_search.sh 
 
-PARAM_DIR='jade_test_params_03/'
-LINES=`ls -1 | wc -l < $PARAM_DIR`
-
-
-for i in $(seq ${SLURM_ARRAY_TASK_ID} 30 $LINES); 
+PARAM_DIR='rawHD_coarse_weight_sweep'
+FILES=(`ls $PARAM_DIR/*.json`)
+NUM_FILES=${#FILES[@]}
+echo "NUM_FILES: $NUM_FILES"
+for i in $(seq $SLURM_ARRAY_TASK_ID 30 $NUM_FILES) 
 do
-    ARGS=`head -$i $PARAM_DIR | tail -1`
-    python HD_eventprop_real_time.py $PARAM_DIR/$ARGS 
-
+    echo "test: ${FILES[$i]}"
+    # python HD_eventprop_run.py ${FILES[$i]} 
 done
