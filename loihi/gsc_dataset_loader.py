5eae203f02bc9cafd477998d2f50aa79d7c0fb1e
thomasshoesmith <thomas.shoesmith@yahoo.com>
2024-06-28 18:18:39 +0100

    update, shuffle sorted, just needs better pretrained weights and alpha in LIF checking

--- loihi/gsc_dataset_loader.py
+++ loihi/gsc_dataset_loader.py
@@ -20,56 +20,43 @@ def load_gsc(dataset_directory,
     params["NETWORK_SCALE"] = NETWORK_SCALE
     params["NUM_INPUT"] = NUM_INPUT
 
-    x_train = np.load(os.path.expanduser(params.get("dataset_directory")) + "training_x_data.npy")
-    y_train = np.load(os.path.expanduser(params.get("dataset_directory")) + "training_y_data.npy")
+    train_x = np.load(os.path.expanduser(params.get("dataset_directory")) + "training_x_data.npy")
+    train_y = np.load(os.path.expanduser(params.get("dataset_directory")) + "training_y_data.npy")
     
-    x_test = np.load(os.path.expanduser(params.get("dataset_directory")) + "testing_x_data.npy")
-    y_test = np.load(os.path.expanduser(params.get("dataset_directory")) + "testing_y_data.npy")
+    test_x = np.load(os.path.expanduser(params.get("dataset_directory")) + "testing_x_data.npy")
+    test_y = np.load(os.path.expanduser(params.get("dataset_directory")) + "testing_y_data.npy")
     
     #TODO: Redundant? any point to have a NUM_INPUT if data can be obtained through dataset shape
-    assert x_train.shape[1] == params.get("NUM_INPUT"), "dataset input size doesn't match passed input parameter size"
+    assert train_x.shape[1] == params.get("NUM_INPUT"), "dataset input size doesn't match passed input parameter size"
     
     if params.get("NETWORK_SCALE") < 1:
-        assert len(x_train) == len(y_train)
-        p = np.random.permutation(len(x_train))
-        x_train, y_train = x_train[p], y_train[p]
-        print(f"original network size: {len(x_train)}")
-        x_train = x_train[:int(len(x_train) * params.get("NETWORK_SCALE"))]
-        y_train = y_train[:int(len(y_train) * params.get("NETWORK_SCALE"))]
-        print(f"reduced network size: {len(x_train)}")
+        assert len(train_x) == len(train_y)
+        p = np.random.permutation(len(train_x))
+        train_x, train_y = train_x[p], train_y[p]
+        print(f"original network size: {len(train_x)}")
+        train_x = train_x[:int(len(train_x) * params.get("NETWORK_SCALE"))]
+        train_y = train_y[:int(len(train_y) * params.get("NETWORK_SCALE"))]
+        print(f"reduced network size: {len(train_x)}")
         print("!! network reduced")
 
-    training_images = np.swapaxes(x_train, 1, 2) 
-    testing_images = np.swapaxes(x_test, 1, 2) 
+    # swap axes (maybe need to transpose)
+    train_x = np.swapaxes(train_x, 1, 2) 
+    test_x = np.swapaxes(test_x, 1, 2) 
  
-    training_images = training_images + abs(np.floor(training_images.min()))
-    testing_images = testing_images + abs(np.floor(testing_images.min()))
-
-    training_labels = y_train
-    testing_labels = y_test
+    # move values into positive
+    train_x = train_x + abs(np.floor(train_x.min()))
+    test_x = test_x + abs(np.floor(test_x.min()))
     
     # adding validation data if exists
-    validation_images = np.array([])
-    validation_labels = np.array([])
+    validation_x = np.array([])
+    validation_y = np.array([])
     if os.path.isfile(os.path.expanduser(params.get("dataset_directory")) + "validation_y_data.npy"):
         print("!! validation dataset loaded successfully")
-        x_validation = np.load(os.path.expanduser(params.get("dataset_directory")) + "validation_x_data.npy")
-        y_validation = np.load(os.path.expanduser(params.get("dataset_directory")) + "validation_y_data.npy")
+        validation_x = np.load(os.path.expanduser(params.get("dataset_directory")) + "validation_x_data.npy")
+        validation_y = np.load(os.path.expanduser(params.get("dataset_directory")) + "validation_y_data.npy")
         
-        validation_images = np.swapaxes(x_validation, 1, 2) 
-        validation_images = validation_images + abs(np.floor(validation_images.min()))
-        
-        validation_labels = y_validation
-
-    # crop the num of samples
-    train_x, train_y = training_images[:num_samples], training_labels[:num_samples]
-    validation_x, validation_y = validation_images[:num_samples], validation_labels[:num_samples]
-    test_x, test_y = testing_images[:num_samples], testing_labels[:num_samples]
-
-    # repeat frames for current injection over (frame) time
-    train_x = np.repeat(train_x, num_frames, axis = 1)
-    validation_x = np.repeat(validation_x, num_frames, axis = 1)
-    test_x = np.repeat(test_x, num_frames, axis = 1)
+        validation_x = np.swapaxes(validation_x, 1, 2) 
+        validation_x = validation_x + abs(np.floor(validation_x.min()))
 
     # shuffle array before stacking (stacking like ring buffer) 
     shuffler = np.random.permutation(len(train_x))
@@ -81,6 +68,16 @@ def load_gsc(dataset_directory,
     shuffler = np.random.permutation(len(test_x))
     test_x = test_x[shuffler]
     test_y = test_y[shuffler]
+
+    # crop the num of samples
+    train_x, train_y = train_x[:num_samples], train_y[:num_samples]
+    validation_x, validation_y = validation_x[:num_samples], validation_y[:num_samples]
+    test_x, test_y = test_x[:num_samples], test_y[:num_samples]
+
+    # repeat frames for current injection over (frame) time
+    train_x = np.repeat(train_x, num_frames, axis = 1)
+    validation_x = np.repeat(validation_x, num_frames, axis = 1)
+    test_x = np.repeat(test_x, num_frames, axis = 1)
 
     # scale values 
     train_x = train_x * scale_value
